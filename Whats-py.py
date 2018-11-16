f0115b019ea48fd935c046dbc569b29f23071be4
Asish Shaji Thomas <mrkai1253@gmail.com>
2018-11-16 14:32:47 +0530

    Initial commit

---
+++ Whats-py.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Whatspy:
+    def __init__(self, sleep_time):
+        '''
+        :param sleep_time: latency used for the purpose to scan qr code
+
+        '''
+
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.get("https://web.whatsapp.com")
+        time.sleep(sleep_time)
+        print('[ ] Logged in')
+
+    def send_message(self, receiver, message):
+        search_box = self.driver.find_element_by_class_name('copyable-text')
+        search_box.send_keys(receiver + Keys.ENTER)
+        message_box = self.driver.find_element_by_class_name('_2S1VP')
+        message_box.send_keys(message + Keys.ENTER)
+        self.driver.save_screenshot('{0}.png'.format(time.time()))
+
+    def quit_browser(self):
+        self.driver.quit()
+
+
+# test
+whatspy = Whatspy(6)
+whatspy.send_message('Anandhu Palayat', 'Odd')
